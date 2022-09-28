5bccaec71e9770e10fa1e1009597a5a70cbf6a60
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-09-28 17:28:54 -0600

    Propiedades en la clase Combatientes con sus getters y setters

--- src/Combatientes.java
+++ src/Combatientes.java
@@ -1,3 +1,30 @@
 public class Combatientes {
 
+    private String name;
+    private int pointsLife;
+    private int powerAttack;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPointsLife() {
+        return pointsLife;
+    }
+
+    public void setPointsLife(int pointsLife) {
+        this.pointsLife = pointsLife;
+    }
+
+    public int getPowerAttack() {
+        return powerAttack;
+    }
+
+    public void setPowerAttack(int powerAttack) {
+        this.powerAttack = powerAttack;
+    }
 }
