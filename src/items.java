04befbbe356c8dc8ffa9df80cd6dcb6900b177cf
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-09-28 17:39:44 -0600

    Creación de las clases items junto a sus propiedades y getters y setters y specialHability

---
+++ src/items.java
@@ -0,0 +1,21 @@
+public class items {
+
+    private String name;
+    private int moreLife;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoreLife() {
+        return moreLife;
+    }
+
+    public void setMoreLife(int moreLife) {
+        this.moreLife = moreLife;
+    }
+}
