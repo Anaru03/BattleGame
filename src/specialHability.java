aa12b061863f68d12f41711961a2b016af584880
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-10-01 14:26:01 -0600

    Se desarrroló los atributos y métodos de la clase specialHability junto con sus getters y setters.

--- src/specialHability.java
+++ src/specialHability.java
@@ -1,3 +1,38 @@
 public class specialHability {
 
+    private String name;
+    private int cure;
+    private int damageLife;
+
+
+    public specialHability (String name, int cure, int damageLife){
+        this.name = name;
+        this.cure = cure;
+        this.damageLife = damageLife;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCure() {
+        return cure;
+    }
+
+    public void setCure(int cure) {
+        this.cure = cure;
+    }
+
+    public int getDamageLife() {
+        return damageLife;
+    }
+
+    public void setDamageLife(int damageLife) {
+        this.damageLife = damageLife;
+    }
 }
