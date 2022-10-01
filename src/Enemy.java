fad5aae190ac2f1dba7ab53c621c59f0ecaa1dd7
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-10-01 14:44:44 -0600

    Desarrollo de las propiedades de la clase Enemy junto sus getteres y setters.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,21 @@
 public class Enemy extends Combatientes{
+
+    private String printMessage;
+    private specialHability hability;
+
+    public String getPrintMessage() {
+        return printMessage;
+    }
+
+    public void setPrintMessage(String printMessage) {
+        this.printMessage = printMessage;
+    }
+
+    public specialHability getHability() {
+        return hability;
+    }
+
+    public void setHability(specialHability hability) {
+        this.hability = hability;
+    }
 }
