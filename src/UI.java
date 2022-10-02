b8459e7d350ef604f213f112d456d57ead1575a7
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-10-01 22:31:41 -0600

    Se creo una clase llamada UIturnoPlayer, donde se usó un printIn que le dará indicaciones sobre el jugador.
    Se desarrolló la clase typeOfState, que dará a conocer mensajes para conocer el estado del combatiente.

--- src/UI.java
+++ src/UI.java
@@ -38,6 +38,28 @@ static Scanner lector = new Scanner(System.in);
         System.out.println(Controller.arrayEnemy.get(numEnemy).getName());
         System.out.println("Tu enemigo te desea comunicar que:  = " + Controller.arrayEnemy.get(numEnemy).getPrintMessage());
     }
+
+    public static int UIturnoPlayer(Player player){
+        System.out.println(" -_- Es tu turno Player -_- ");
+        System.out.println("Escriba el no. de la opcción seleccioanda");
+        System.out.println("1 Hacer uso de la habilidad especial \n2 Atacar al oPONENTE" );
+        int battle = lector.nextInt();
+        if(battle == 1){
+            System.out.println("The Player: " + player.getName() + "invocó el ataque, ocasioanando: " + player.getPowerAttack() + "puntos de daño");
+        }
+        else {
+            System.out.println("The Player" + player.getName() + "ha invocando su ítem: " + player.getHabilidades().getName() + "ocasioanado: ");
+            System.out.println("Daño ocasioando: " + player.getHabilidades().getAttack());
+            System.out.println("Curación: " + player.getHabilidades().getMoreLife());
+        }
+
+        return battle;
+    }
+
+    public static void typeOfState(Combatientes combatientes){
+        System.out.println("El combatiente: " + combatientes.getName() + "obtiene: ");
+        System.out.println("Tiene vidad: " + combatientes.getPointsLife());
+    }
 }
 
 
